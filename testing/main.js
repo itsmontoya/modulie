fd84b07a9316e358e997be1f922f83808e97a355
itsMontoya <josh@montoya.io>
2017-04-25 12:22:33 -0700

    Adding mapped import to main.js

--- testing/main.js
+++ testing/main.js
@@ -1,6 +1,6 @@
 function main() {
 	modulie.Import([
-		new modulie.Entry("foo.js"), // Imports the foo module as "foo"
+		new modulie.Entry("foo.js", null), // Imports the foo module as "foo"
 		new modulie.Entry("foo.js", "foo", "foo2") // Allows to import the foo module as "foo2"
 	], onLoad, onError);
 
