9ac4b8fbde61c0c91e34b1a32c66d3c0ca666606
itsMontoya <josh@montoya.io>
2017-04-25 12:21:44 -0700

    Adding bar module and module mapping to foo.js

--- testing/foo.js
+++ testing/foo.js
@@ -1,11 +1,26 @@
 var foo = (function(){
-	function Bar(){
-		console.log("bar!");
+	function Greet(){
+		console.log("Hello world! This is foo");
 	}
 
 	function Exports(){
-		this.Bar = Bar;
+		this.Greet = Greet;
 	}
 
 	return new Exports;
 })();
+
+var bar = (function(){
+	function Greet(){
+		console.log("Hello world! This is bar");
+	}
+
+	function Exports(){
+		this.Greet = Greet;
+	}
+
+	return new Exports;
+})();
+
+var __map = __map || {};
+__map["foo.js"] = ["foo","bar"];
